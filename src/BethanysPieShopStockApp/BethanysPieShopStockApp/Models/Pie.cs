3a6e51b4d4497bbcc25bf6e15d3be6a01fc5aa5c
s v <chofexx@gmail.com>
2020-11-13 08:15:02 +0200

    Adding Base View Model -> implements INotifyPropertyChanged

--- src/BethanysPieShopStockApp/BethanysPieShopStockApp/Models/Pie.cs
+++ src/BethanysPieShopStockApp/BethanysPieShopStockApp/Models/Pie.cs
@@ -1,12 +1,71 @@
-﻿namespace BethanysPieShopStockApp.Models
+﻿using BethanysPieShopStockApp.ViewModels;
+
+namespace BethanysPieShopStockApp.Models
 {
-    public class Pie
+    public class Pie : BaseViewModel
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Price { get; set; }
-        public string Description { get; internal set; }
-        public bool InStock { get; set; }
-        public string ImageURI { get; set; }
+        private string _name;
+        private double _price;
+        private string _description;
+        private bool _inStock;
+        private string _imageURI;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value != null)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+                
+            }
+        }
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                _price = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                if (value != null)
+                {
+                    _description = value;
+                    OnPropertyChanged();
+                }
+               
+            }
+        }
+        public bool InStock
+        {
+            get => _inStock;
+            set
+            {
+                _inStock = value;
+                OnPropertyChanged();
+            }
+        }
+        public string ImageURI
+        {
+            get => _imageURI;
+            set
+            {
+                if (value != null)
+                {
+                    _imageURI = value;
+                    OnPropertyChanged();
+                }
+               
+            }
+        }
     }
 }
---
+++ src/BethanysPieShopStockApp/BethanysPieShopStockApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BethanysPieShopStockApp.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set 
+            { 
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

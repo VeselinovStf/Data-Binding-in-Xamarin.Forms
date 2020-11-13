412538245bb70130c4536602b9b2d4769558860f
s v <chofexx@gmail.com>
2020-11-13 07:33:42 +0200

    Add Pie infrastructure

---
+++ src/BethanysPieShopStockApp/BethanysPieShopStockApp/Models/Pie.cs
@@ -0,0 +1,12 @@
+﻿namespace BethanysPieShopStockApp.Models
+{
+    public class Pie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; internal set; }
+        public bool InStock { get; set; }
+        public string ImageURI { get; set; }
+    }
+}
---
+++ src/BethanysPieShopStockApp/BethanysPieShopStockApp/Services/MockPieService.cs
@@ -0,0 +1,119 @@
+﻿using BethanysPieShopStockApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BethanysPieShopStockApp.Services
+{
+    /// <summary>
+    /// Mock Service for Pies
+    /// </summary>
+    public class MockPieService
+    {
+        private IList<Pie> _pies;
+
+        public MockPieService()
+        {
+            InitializePies();
+        }
+
+        private void InitializePies()
+        {
+            this.Pies = new List<Pie>()
+            {
+                new Pie()
+                {
+                     Id = 1,
+                     Name = "Apple Pie",
+                     Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.",
+                     InStock = true,
+                     Price = 123,
+                     ImageURI = "piesImg.png"
+                },
+                new Pie()
+                {
+                     Id = 2,
+                     Name = "Chery Pie",
+                     Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.",
+                     InStock = true,
+                     Price = 234,
+                     ImageURI = "piesImg.png"
+                },
+                new Pie()
+                {
+                     Id = 3,
+                     Name = "Pine Pie",
+                     Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.",
+                     InStock = true,
+                     Price = 432,
+                     ImageURI = "piesImg.png"
+                },
+                new Pie()
+                {
+                     Id = 4,
+                     Name = "Orange Pie",
+                     Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.",
+                     InStock = true,
+                     Price = 234,
+                     ImageURI = "piesImg.png"
+                },
+                new Pie()
+                {
+                     Id = 5,
+                     Name = "Code Pie",
+                     Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.",
+                     InStock = true,
+                     Price = 654,
+                     ImageURI = "piesImg.png"
+                },
+                new Pie()
+                {
+                     Id = 6,
+                     Name = "Pizza Pie",
+                     Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.",
+                     InStock = true,
+                     Price = 76,
+                     ImageURI = "piesImg.png"
+                },
+                new Pie()
+                {
+                     Id = 7,
+                     Name = "Hulk Pie",
+                     Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.",
+                     InStock = true,
+                     Price = 321,
+                     ImageURI = "piesImg.png"
+                },
+                new Pie()
+                {
+                     Id = 8,
+                     Name = "Mobile Pie",
+                     Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.",
+                     InStock = true,
+                     Price = 543,
+                     ImageURI = "piesImg.png"
+                },
+            };
+
+        }
+
+        public IList<Pie> Pies
+        {
+            get => _pies.OrderBy(p => p.Id).ToList();
+            private set => _pies = value;
+        }
+
+        public Pie Get(int id)
+        {
+            return this.Pies.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Update(Pie pie)
+        {
+            var updatablePie = this.Pies.FirstOrDefault(p => p.Id == pie.Id);
+            updatablePie = pie;
+        }
+
+
+    }
+}

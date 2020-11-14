1f72787a7c0d142224bd3c4b1aa169a72af53591
s v <chofexx@gmail.com>
2020-11-14 08:17:14 +0200

    Adding Converter -> Color the Price based on value

---
+++ src/BethanysPieShopStockApp/BethanysPieShopStockApp/Converters/DoubleToColorConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BethanysPieShopStockApp.Converters
+{
+    public class DoubleToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double p = 200;
+            if (parameter != null)
+            {
+                p = Double.Parse(parameter.ToString());
+            }
+
+
+            switch (value)
+            {
+                case double v when v > p:
+                    return Color.Gold;
+                default:
+                    return Color.Red;                    
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}

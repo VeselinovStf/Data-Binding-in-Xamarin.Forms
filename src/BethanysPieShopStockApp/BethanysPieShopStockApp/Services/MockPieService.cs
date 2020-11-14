5546baedb07992ccd615eba4bf8152b2e0f03015
s v <chofexx@gmail.com>
2020-11-14 07:46:36 +0200

    Adding Custom DataTemplateSelector

--- src/BethanysPieShopStockApp/BethanysPieShopStockApp/Services/MockPieService.cs
+++ src/BethanysPieShopStockApp/BethanysPieShopStockApp/Services/MockPieService.cs
@@ -36,7 +36,7 @@ namespace BethanysPieShopStockApp.Services
                      Id = 2,
                      Name = "Chery Pie",
                      Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.",
-                     InStock = true,
+                     InStock = false,
                      Price = 234,
                      ImageURI = "applepie.jpg",
                      AvailibleDate = DateTime.Today
---
+++ src/BethanysPieShopStockApp/BethanysPieShopStockApp/TemplatesSelectors/PieTemplateSelector.cs
@@ -0,0 +1,17 @@
+﻿using BethanysPieShopStockApp.Models;
+using Xamarin.Forms;
+
+namespace BethanysPieShopStockApp.TemplatesSelectors
+{
+    public class PieTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate RegularPieTemplate { get; set; }
+
+        public DataTemplate NotInStockTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            return ((Pie)item).InStock ? RegularPieTemplate : NotInStockTemplate;
+        }
+    }
+}

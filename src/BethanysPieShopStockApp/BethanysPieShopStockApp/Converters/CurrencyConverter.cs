126d70d7d052873352df1de35bee105d66b67135
s v <chofexx@gmail.com>
2020-11-14 08:02:58 +0200

    Adding Converter - to add $ sign to prices

---
+++ src/BethanysPieShopStockApp/BethanysPieShopStockApp/Converters/CurrencyConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace BethanysPieShopStockApp.Converters
+{
+    public class CurrencyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"$ {value.ToString()}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString().Replace("$", string.Empty).Trim();
+        }
+    }
+}
